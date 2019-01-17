06066a51bceb6f33e12310bd16075a9eaeb8a52c
nimeshkrishnani <nimesh.krishnani@iiitb.org>
2019-01-17 10:13:34 +0530

    first calc java

---
+++ calcadd.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class calcadd
+{
+	public static void main(String args[])
+	{
+		float a, b, res;
+		char choice, ch;
+		Scanner scan = new Scanner(System.in);
+		
+		do
+		{
+			System.out.println("1.Add Two Numbers");
+			System.out.println("2.Exit");
+			System.out.print("Enter your choice:");
+			choice = scan.next().charAt(0);
+			switch(choice)
+			{
+				case '1' : System.out.println("Enter Two numbers:");
+					a = scan.nextFloat();
+					b = scan.nextFloat();
+					res = a+b;
+					System.out.println("Your result is:"+res);
+					break;
+				case '2' : System.exit(0);
+					break;
+				default : System.out.println("INVALID CHOICE");
+			}
+			System.out.print("\n---------------------------------------\n");
+		}
+		while(choice!='2');
+	}
+}
